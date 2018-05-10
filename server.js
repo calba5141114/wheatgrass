f688a5c235654d6a30aeb04d62157136bbb3819c
Haley Levesque <CARLOSalba989@gmail.com>
2018-05-09 21:34:38 -0700

    just carlos experiments

--- server.js
+++ server.js
@@ -6,13 +6,37 @@ app.use(express.static('public'));
 app.set("view engine", "pug");
 app.set("views", __dirname +"/public/views");
 
+// sending off the index.page
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+// sending off the about page
 app.get('/about', (req, res) => {
     res.render('about');
 });
+
+
+// retrieving user data from front end on form click.
+app.get('/process_get', (req, res) =>{
+    response = {
+        first_name : req.query.first_name,
+        last_name : req.query.last_name,
+        gender: req.query.gender
+        };
+
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000');
